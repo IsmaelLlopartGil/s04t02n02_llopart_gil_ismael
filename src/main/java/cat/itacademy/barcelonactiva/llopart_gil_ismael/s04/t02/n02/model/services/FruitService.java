bbe50038da7999e69fdf343cf2f668b1476321fc
IsmaelLG <ismael.llopart@gmail.com>
2022-11-14 12:07:09 +0100

    service extension point

--- src/main/java/cat/itacademy/barcelonactiva/llopart_gil_ismael/s04/t02/n02/model/services/FruitService.java
+++ src/main/java/cat/itacademy/barcelonactiva/llopart_gil_ismael/s04/t02/n02/model/services/FruitService.java
@@ -2,31 +2,15 @@ package cat.itacademy.barcelonactiva.llopart_gil_ismael.s04.t02.n02.model.servic
 
 import java.util.List;
 import java.util.Optional;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
 import cat.itacademy.barcelonactiva.llopart_gil_ismael.s04.t02.n02.model.domain.Fruit;
-import cat.itacademy.barcelonactiva.llopart_gil_ismael.s04.t02.n02.model.repository.FruitRepository;
 
-@Service
-public class FruitService {
+public interface FruitService {
 
-	@Autowired
-	FruitRepository fruitRepository;
+	public void save(Fruit fruit);
 
-	public void save(Fruit fruit) {
-		fruitRepository.save(fruit);
-	}
+	public Optional<Fruit> findById(int id);
 
-	public Optional<Fruit> findById(int id) {
-		return fruitRepository.findById(id);
-	}
+	public void deleteById(int id);
 
-	public void deleteById(int id) {
-		fruitRepository.deleteById(id);
-	}
-
-	public List<Fruit> findAll() {
-		return (List<Fruit>) fruitRepository.findAll();
-	}
-
+	public List<Fruit> findAll();
 }
